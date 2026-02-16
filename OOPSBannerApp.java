7a8047c095e90f534769ef740010506c562e7ae3
Vedasri Madana <madanavedasri19@gmail.com>
2026-02-17 00:29:26 +0530

    UC2: Render OOPS banner using string concatenation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,10 @@
-/**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System OOPS acronym to the console.
- *
- * @author Vedasri
- * @version 1.0
- */
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****     *****     *****     ***** ");
+        System.out.println("*     *   *     *   *     *   *      ");
+        System.out.println("*     *   *     *    *****     ***** ");
+        System.out.println("*     *   *     *   *              * ");
+        System.out.println(" *****     *****    *         ***** ");
     }
 }
